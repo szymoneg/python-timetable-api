d0094fecdf227ca4d1384bc8fc5c00cfae066375
Szymon <szymon.zsp5@gmail.com>
2021-02-10 21:26:24 +0100

    +find by direction, find by closest time

--- timetable/urls.py
+++ timetable/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.server_test),
-    path('timetables/', views.get_response_timetable_solak)
+    path('timetables/', views.get_response_timetable_solak),
+    path('direction/<str:direction>', views.get_timetable_solak_oneway),
+    path('time/<str:time>/<str:direction>', views.get_close_time)
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework import status
+import datetime
 
 
 def server_test(request):
@@ -14,3 +15,24 @@ def server_test(request):
 def get_response_timetable_solak(request):
     timetable_get = get_timetable_solak()
     return JsonResponse(timetable_get, safe=False)
+
+
+@api_view(['GET'])
+def get_timetable_solak_oneway(request, direction):
+    timetable_get = get_timetable_solak()[direction]
+    return JsonResponse(timetable_get, safe=False)
+
+
+@api_view(['GET'])
+def get_close_time(request, time, direction):
+    day = datetime.datetime.now().weekday()
+    if 0 < day <= 4:
+        hours = list(get_timetable_solak()[direction][0])
+    elif day == 5:
+        hours = list(get_timetable_solak()[direction][1])
+    else:
+        hours = list(get_timetable_solak()[direction][2])
+    now = datetime.datetime.strptime(time, "%H:%M")
+    # VVV
+    closest_bus = min(hours, key=lambda t: abs(now - datetime.datetime.strptime(t, "%H:%M")))
+    return JsonResponse(closest_bus, safe=False)
